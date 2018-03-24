ce100b5e0a4522331dd87c90e99a8e72e449d4ae
Sud <sudhanshuambastha@gmail.com>
2018-03-24 12:41:33 +0530

    show map with my location search added

--- app/src/main/java/inc/iris/sih2018/MapActivity.java
+++ app/src/main/java/inc/iris/sih2018/MapActivity.java
@@ -1,24 +1,17 @@
 package inc.iris.sih2018;
 
 import android.Manifest;
-import android.app.Dialog;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
