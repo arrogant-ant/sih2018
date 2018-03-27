5d5a1dd8f564a83236cc458365a669b0a3029a52
Sud <sudhanshuambastha@gmail.com>
2018-03-27 21:25:13 +0530

    StringRequest

--- app/src/main/java/inc/iris/sih2018/logic/Parse.java
+++ app/src/main/java/inc/iris/sih2018/logic/Parse.java
@@ -1,21 +1,42 @@
-/*
 package inc.iris.sih2018.logic;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-*/
 /**
  * Created by Sud on 3/27/18.
- *//*
+ */
 
 
-public class Parse<T> {
-    public  T[] fromJSON(JSONObject json, T t)
-    {
+public class Parse {
+    public static Booking[] getBooking(JSONObject json, BookingStatus status) {
+        int i;
+        try {
+            JSONArray array = json.getJSONArray("server_response");
+            Booking[] list = new Booking[array.length()];
+            for (i = 0; i < array.length(); i++) {
+                JSONObject object = array.getJSONObject(i);
+                list[i] = new Booking(
+                        object.getLong("arrival"), //arrival
+                        object.getLong("departure"), //
+                        new ParkingSlot(
+                                object.getString("slotID"),
+                                object.getString("name"),
+                                object.getString("address"),
+                                object.getString("gps"),
+                                object.getInt("capacity"),
+                                object.getInt("slotID")
+                        ),
+                        object.getString("vehicleID"),
+                        status);
+            }
+            return list;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-        String key[]=t.getKey();
-
+        return null;
 
     }
 }
-*/
